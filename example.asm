08cf41f8a6ff38e8e6ce2bc399daaa2d79077084
disqet <71570574+disqet@users.noreply.github.com>
2022-04-25 00:09:14 +0300

    Create example.asm

---
+++ example.asm
@@ -0,0 +1,3 @@
+MOV DL,[2400]          ;DS:2400H adresindeki veriyi DL adresine aktarır.
+MOV [DI],AH            ;AH adresindeki veriyi DS:DI adresine aktarır.
+MOV CX,[BX] + 10       ;DS:BX + 10 adresindeki veriyi CX adresine aktarır.
